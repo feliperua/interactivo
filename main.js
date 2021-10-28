735153a2bef2028c4861d5db5f3eaad7d0bc1665
maolink <maolink@yahoo.com>
2021-10-27 22:44:58 -0500

    Organizando funciones

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ const rojo= document.getElementById('rojo')
 const azul= document.getElementById('azul')
 const amarillo= document.getElementById('amarillo')
 
-cambio.addEventListener('click',()=>{
-    //inter.innerText='nuevo titulo'innertext tenemos un camnbio en el texto
-    const nuevotitulo=titulo.value
-    inter.innerText=nuevotitulo
-    //inter.style.color='blue'
-} )
+cambio.addEventListener('click',()=>manejadorTitulo() )
+
+
+
 rojo.addEventListener('click',()=>{
     inter.style.color="red"
 })
@@ -23,3 +21,9 @@ amarillo.addEventListener('click',()=>{
     inter.style.color="yellow"
 })
 
+const manejadorTitulo = () =>{
+            //inter.innerText='nuevo titulo'innertext tenemos un camnbio en el texto
+        const nuevotitulo=titulo.value
+        inter.innerText=nuevotitulo
+        //inter.style.color='blue'
+}

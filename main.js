93fb938608b340ed7f274fe90b8fdc7aca6294b4
feliperua <felipe2220@example.com>
2021-10-27 22:37:40 -0500

    interactivo

---
+++ main.js
@@ -0,0 +1,25 @@
+const inter= document.getElementById('tivo')
+const titulo= document.getElementById('titulo')
+const cambio= document.getElementById('cambio')
+const rojo= document.getElementById('rojo')
+const azul= document.getElementById('azul')
+const amarillo= document.getElementById('amarillo')
+
+cambio.addEventListener('click',()=>{
+    //inter.innerText='nuevo titulo'innertext tenemos un camnbio en el texto
+    const nuevotitulo=titulo.value
+    inter.innerText=nuevotitulo
+    //inter.style.color='blue'
+} )
+rojo.addEventListener('click',()=>{
+    inter.style.color="red"
+})
+
+azul.addEventListener('click',()=>{
+    inter.style.color="blue"
+})
+
+amarillo.addEventListener('click',()=>{
+    inter.style.color="yellow"
+})
+
